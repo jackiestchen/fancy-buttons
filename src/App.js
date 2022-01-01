a6f075032faf88e3c70b234aa7d6479abd82d298
jackiestchen <jackiestchen@gmail.com>
2022-01-01 16:04:45 -0700

    Import components to main App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
   return (
     <div className="App">
-
+      <AngryButton />
+      <CounterButton />
+      <LightSwitchButton />
+      <TextRepeaterButton />
     </div>
   );
 }
